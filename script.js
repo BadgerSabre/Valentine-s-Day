04e574b30985baa8f3cd9bed6eecfdc3e87f33b5
Brandon Dega <brandondega@gmail.com>
2022-02-10 11:00:23 -0600

    html done, js half done

---
+++ script.js
@@ -0,0 +1,31 @@
+let buttonOne = document.querySelector('#buttonOne')
+let buttonTwo = document.querySelector('#buttonTwo')
+let buttonThree = document.querySelector('#buttonThree')
+let buttonFour = document.querySelector('#buttonFour')
+let buttonFive = document.querySelector('#buttonFive')
+
+let messageOne = document.querySelector('#messageOne')
+let messageTwo = document.querySelector('#messageTwo')
+let messageThree = document.querySelector('#messageThree')
+let messageFour = document.querySelector('#messageFour')
+let messageFive = document.querySelector('#messageFive')
+
+buttonOne.addEventListener("click",function(){
+    messageOne.textContent = 'You are a Master Thief!'
+})
+
+buttonTwo.addEventListener("click",function(){
+    messageTwo.textContent = 'Tu est mon partenaire pour parler francais.'
+})
+
+buttonThree.addEventListener("click",function(){
+    
+})
+
+buttonFour.addEventListener("click",function(){
+
+})
+
+buttonFive.addEventListener("click",function(){
+
+})
